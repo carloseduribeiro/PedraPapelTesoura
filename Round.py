6108c5f5721b90682630d01876a735d54b4f2702
Carlos Eduardo <carloseribeiro96@gmail.com>
2021-04-14 10:28:05 -0300

    Método set_winner() adicionado; Método start modificado para usar o set_winner() e o get_hand_by_round() da classe Player; Atribus desnecessários removidos;

--- Round.py
+++ Round.py
@@ -3,47 +3,44 @@ from Player import Player
 
 class Round:
     winner: Player
-    hand_p1: int
-    hand_p2: int
-    hands = {
-        1: "PEDRA",
-        2: "PAPEL",
-        3: "TESOURA",
-    }
 
-    def __init__(self, p1: Player, p2: Player, num_round: int):
-        self.winner = Player("EMPATE")
+    def __init__(self, p1: Player, p2: Player, num_round):
         self.player_1 = p1
         self.player_2 = p2
         self.num_round = num_round
 
+    def set_winner(self, winner: Player):
+        self.winner = winner
+        self.winner.add_score()
+
     def start(self):
-        self.hand_p1 = self.player_1.get_hands()[self.num_round]
-        self.hand_p2 = self.player_2.get_hands()[self.num_round]
+        hand_p1 = self.player_1.get_hand_by_round(self.num_round)
+        hand_p2 = self.player_2.get_hand_by_round(self.num_round)
 
         # PEDRA | PAPEL | TESOURA
         # 1 | 2 | 3
-
-        if self.hand_p1 == 1 and self.hand_p2 == 3:
+        if hand_p1 == 1 and hand_p2 == 3:
             # PEDRA vs TESOURA = PEDRA
-            self.winner = self.player_1
-        elif self.hand_p1 == 1 and self.hand_p2 == 2:
+            self.set_winner(self.player_1)
+        elif hand_p1 == 1 and hand_p2 == 2:
             # PEDRA vs PAPEL = PAPEL
-            self.winner = self.player_2
+            self.set_winner(self.player_2)
 
-        if self.hand_p1 == 2 and self.hand_p2 == 1:
+        elif hand_p1 == 2 and hand_p2 == 1:
             # PAPEL vs PEDRA = PAPEL
-            self.winner = self.player_1
-        elif self.hand_p1 == 2 and self.hand_p2 == 3:
+            self.set_winner(self.player_1)
+        elif hand_p1 == 2 and hand_p2 == 3:
             # PAPEL vs TESOURA = TESOURA
-            self.winner = self.player_2
+            self.set_winner(self.player_2)
 
-        elif self.hand_p1 == 3 and self.hand_p2 == 2:
+        elif hand_p1 == 3 and hand_p2 == 2:
             # TESOURA vs PAPEL = TESOURA
-            self.winner = self.player_1
-        elif self.hand_p1 == 3 and self.hand_p2 == 1:
+            self.set_winner(self.player_1)
+        elif hand_p1 == 3 and hand_p2 == 1:
             # TESOURA vs PEDRA = PEDRA
-            self.winner = self.player_2
+            self.set_winner(self.player_2)
+        else:
+            self.set_winner(Player("Empate"))
 
-    def get_winner(self):
+    def get_winner(self) -> Player:
         return self.winner
